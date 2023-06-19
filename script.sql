4f47ed83bc45b377835fa8f9ec568271cd61f7ff
Atul-sagar <atulsagar55@gmail.com>
2023-06-19 15:18:41 +0530

    updated script.sql

--- script.sql
+++ script.sql
@@ -1,3 +1,23 @@
+-- create database cobrowsing;
+
+-- use cobrowsing;
+
+-- create table Actions(
+-- 	ID int AUTO_INCREMENT PRIMARY KEY,
+--     name varchar(50),
+--     value varchar(50)
+-- )
+
+-- select * from Actions;
+-- INSERT INTO Actions (name,value) VALUES ("atool","toolbox");
+-- INSERT INTO Actions (name,value) VALUES ("name","Atul");
+-- INSERT INTO Actions (name,value) VALUES ("country","USA");
+
+-- select *from Actions order by ID desc limit 1;
+
+-- drop table Actions;
+
+
 create database cobrowsing;
 
 use cobrowsing;
@@ -5,12 +25,14 @@ use cobrowsing;
 create table Actions(
 	ID int AUTO_INCREMENT PRIMARY KEY,
     name varchar(50),
-    value varchar(50)
+    controlType varchar(50),
+    controlID varchar(20),
+    value varchar(5000)
 )
 
 select * from Actions;
 INSERT INTO Actions (name,value) VALUES ("atool","toolbox");
-INSERT INTO Actions (name,value) VALUES ("name","Atul");
+INSERT INTO Actions (name,controlType,value) VALUES ("name","dropdown","Atul");
 INSERT INTO Actions (name,value) VALUES ("country","USA");
 
 select *from Actions order by ID desc limit 1;
